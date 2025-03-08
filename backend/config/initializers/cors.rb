168d4f87767943db8e39e53d6fd58fb70155dd91
riku-work10 <ukir19990530@gmail.com>
2025-03-08 16:20:13 +0900

    a

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV["FRONT_URL"], 'https://dashboard.heroku.com/apps/vercel-heroku-sample'
+    origins ENV["FRONT_URL"], 'https://vercel-heroku-sample-198c1f90b154.herokuapp.com/'
 
     resource "*",
       headers: :any,
